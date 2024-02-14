d5d75b115b778d658f4a9963c71c8e4e575aa434
Don Cosio <dacosio@gmail.com>
2024-02-13 16:26:21 -0800

    update person

--- client/src/components/listItems/CarPersonItem.js
+++ client/src/components/listItems/CarPersonItem.js
@@ -1,12 +1,31 @@
-import { Typography, Container, Grid, Button, Stack } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import {
+  Typography,
+  Container,
+  Grid,
+  Button,
+  Stack,
+  FormControl,
+  TextField,
+} from "@mui/material";
+
 import formatCurrency from "../../utils/formatCurrency";
 import { useMutation } from "@apollo/client";
-import { GET_CARS, REMOVE_CAR, REMOVE_PERSON } from "../../graphql/queries";
+import {
+  GET_PEOPLE,
+  REMOVE_CAR,
+  REMOVE_PERSON,
+  UPDATE_PERSON,
+} from "../../graphql/queries";
 
 const CarPersonItem = ({ id, firstName, lastName, cars }) => {
   const [removeCar] = useMutation(REMOVE_CAR);
   const [removePerson] = useMutation(REMOVE_PERSON);
+  const [updatePerson] = useMutation(UPDATE_PERSON);
+
+  const [edit, setEdit] = useState(false);
+  const [firstVal, setFirstVal] = useState(firstName);
+  const [lastVal, setLastVal] = useState(lastName);
 
   const handleDeleteCar = (id) => {
     let result = window.confirm("Are you sure you want to delete this car?");
@@ -31,8 +50,7 @@ const CarPersonItem = ({ id, firstName, lastName, cars }) => {
     }
   };
 
-  const handleDeletePerson = (id) => {
-    console.log("🚀 ~ handleDeletePerson ~ id:", id);
+  const handleDeletePerson = () => {
     let result = window.confirm("Are you sure you want to delete this car?");
     if (result) {
       removePerson({
@@ -53,6 +71,29 @@ const CarPersonItem = ({ id, firstName, lastName, cars }) => {
       });
     }
   };
+
+  const handleUpdatePerson = () => {
+    updatePerson({
+      variables: {
+        id,
+        firstName: firstVal,
+        lastName: lastVal,
+      },
+      update(cache, { data: { updatePerson } }) {
+        console.log("🚀 ~ update ~ updatePerson:", updatePerson);
+        const data = cache.readQuery({ query: GET_PEOPLE });
+
+        cache.writeQuery({
+          query: GET_PEOPLE,
+          data: {
+            ...data,
+          },
+        });
+        setEdit(false);
+      },
+    });
+  };
+
   return (
     <Container sx={{ marginBottom: 2 }}>
       <Grid
@@ -63,26 +104,76 @@ const CarPersonItem = ({ id, firstName, lastName, cars }) => {
         padding={2}
         sx={{ backgroundColor: "whitesmoke" }}>
         <Grid>
-          <Typography variant="h6">
-            {firstName} {lastName}
-          </Typography>
+          {!edit ? (
+            <Typography variant="h6">
+              {firstName} {lastName}
+            </Typography>
+          ) : (
+            <form>
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <FormControl fullWidth>
+                    <TextField
+                      label="First Name"
+                      value={firstVal}
+                      onChange={(e) => setFirstVal(e.target.value)}
+                      fullWidth
+                      size="small"
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={6}>
+                  <FormControl fullWidth>
+                    <TextField
+                      label="Last Name"
+                      value={lastVal}
+                      onChange={(e) => setLastVal(e.target.value)}
+                      fullWidth
+                      size="small"
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+            </form>
+          )}
         </Grid>
         <Grid>
           <Stack spacing={1} direction="row">
-            <Button
-              variant="contained"
-              color="secondary"
-              size="small"
-              onClick={() => null}>
-              Edit
-            </Button>
-            <Button
-              variant="contained"
-              color="error"
-              size="small"
-              onClick={() => handleDeletePerson(id)}>
-              Delete
-            </Button>
+            {!edit ? (
+              <>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  size="small"
+                  onClick={() => setEdit(!edit)}>
+                  Edit
+                </Button>
+                <Button
+                  variant="contained"
+                  color="error"
+                  size="small"
+                  onClick={handleDeletePerson}>
+                  Delete
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  variant="contained"
+                  color="warning"
+                  size="small"
+                  onClick={handleUpdatePerson}>
+                  Submit
+                </Button>
+                <Button
+                  variant="contained"
+                  color="error"
+                  size="small"
+                  onClick={() => setEdit(false)}>
+                  Cancel
+                </Button>
+              </>
+            )}
           </Stack>
         </Grid>
       </Grid>
