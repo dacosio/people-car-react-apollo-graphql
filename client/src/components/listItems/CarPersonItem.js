f11eaab276f0b830d5df6e9019b18b9bfc89417e
Don Cosio <dacosio@gmail.com>
2024-02-13 21:04:15 -0800

    show car inputs

--- client/src/components/listItems/CarPersonItem.js
+++ client/src/components/listItems/CarPersonItem.js
@@ -7,10 +7,12 @@ import {
   Stack,
   FormControl,
   TextField,
+  Select,
+  MenuItem,
 } from "@mui/material";
 
 import formatCurrency from "../../utils/formatCurrency";
-import { useMutation } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import {
   GET_PEOPLE,
   REMOVE_CAR,
@@ -22,10 +24,20 @@ const CarPersonItem = ({ id, firstName, lastName, cars }) => {
   const [removeCar] = useMutation(REMOVE_CAR);
   const [removePerson] = useMutation(REMOVE_PERSON);
   const [updatePerson] = useMutation(UPDATE_PERSON);
+  const { data } = useQuery(GET_PEOPLE);
 
   const [edit, setEdit] = useState(false);
   const [firstVal, setFirstVal] = useState(firstName);
   const [lastVal, setLastVal] = useState(lastName);
+
+  const [year, setYear] = useState(0);
+  const [make, setMake] = useState("");
+  const [model, setModel] = useState("");
+  const [price, setPrice] = useState("");
+  const [option, setOption] = useState("");
+  const [editCar, setEditCar] = useState(false);
+  const [selectedCarIndex, setSelectedCarIndex] = useState(-1);
+  const [carVal, setCarVal] = useState();
 
   const handleDeleteCar = (id) => {
     let result = window.confirm("Are you sure you want to delete this car?");
@@ -94,6 +106,12 @@ const CarPersonItem = ({ id, firstName, lastName, cars }) => {
     });
   };
   const isFormValid = firstVal.trim() !== "" && lastVal.trim() !== "";
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEditCarId(null);
+  };
+
+  const [editCarId, setEditCarId] = useState(null);
 
   return (
     <Container sx={{ marginBottom: 2 }}>
@@ -180,38 +198,149 @@ const CarPersonItem = ({ id, firstName, lastName, cars }) => {
         </Grid>
       </Grid>
       {cars &&
-        cars.slice(0, 5).map(({ id, year, make, model, price }, idx) => (
-          <Grid
-            container
-            justifyContent="space-between"
-            alignItems="center"
-            marginBottom={1}
-            key={idx}>
-            <Grid item>
-              <Typography marginLeft={2} variant="body1" key={idx}>
-                {year} {make} {model} {formatCurrency(price)}
-              </Typography>
+        cars
+          .slice(0, 5)
+          .map(({ id, year, make, model, price, personId }, idx) => (
+            <Grid
+              container
+              justifyContent="space-between"
+              alignItems="center"
+              marginBottom={1}
+              key={idx}>
+              {!editCarId || editCarId !== id ? (
+                <Grid
+                  container
+                  justifyContent="space-between"
+                  alignItems="center">
+                  <Grid item>
+                    <Typography marginLeft={2} variant="body1" key={id + year}>
+                      {year} {make} {model} {formatCurrency(price)}
+                    </Typography>
+                  </Grid>
+                  <Grid item paddingRight={2}>
+                    <Stack spacing={1} direction="row">
+                      <Button
+                        variant="outlined"
+                        color="secondary"
+                        size="small"
+                        onClick={() => {
+                          setEditCarId(id);
+                        }}>
+                        Edit
+                      </Button>
+                      <Button
+                        variant="outlined"
+                        color="error"
+                        size="small"
+                        onClick={() => handleDeleteCar(id)}>
+                        Delete
+                      </Button>
+                    </Stack>
+                  </Grid>
+                </Grid>
+              ) : (
+                <Grid
+                  container
+                  justifyContent="space-between"
+                  alignItems="center"
+                  flexWrap="nowrap">
+                  {/* <form onSubmit={handleSubmit}> */}
+                  <Grid
+                    container
+                    flex={"row"}
+                    spacing={2}
+                    sx={{ marginBottom: 0 }}>
+                    <Grid item xs={2}>
+                      <FormControl fullWidth>
+                        <TextField
+                          label="Year"
+                          value={year}
+                          onChange={(e) => setYear(e.target.value)}
+                          fullWidth
+                          size="small"
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <FormControl fullWidth>
+                        <TextField
+                          label="Make"
+                          value={make}
+                          onChange={(e) => setMake(e.target.value)}
+                          fullWidth
+                          size="small"
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <FormControl fullWidth>
+                        <TextField
+                          label="Model"
+                          value={model}
+                          onChange={(e) => setModel(e.target.value)}
+                          fullWidth
+                          size="small"
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <FormControl fullWidth>
+                        <TextField
+                          label="Price"
+                          value={price}
+                          onChange={(e) => setPrice(e.target.value)}
+                          fullWidth
+                          size="small"
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <FormControl fullWidth>
+                        <Select
+                          value={personId}
+                          onChange={(e) => setOption(e.target.value)}
+                          displayEmpty
+                          fullWidth
+                          size="small">
+                          <MenuItem value="" disabled>
+                            Select an option
+                          </MenuItem>
+                          {data &&
+                            data.people.map((person, idx) => (
+                              <MenuItem key={idx} value={person.id}>
+                                {person.firstName} {person.lastName}
+                              </MenuItem>
+                            ))}
+                        </Select>
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                  {/* </form> */}
+                  <Grid item paddingRight={2}>
+                    <Stack spacing={1} direction="row">
+                      <Button
+                        variant="outlined"
+                        color="warning"
+                        size="small"
+                        onClick={() => {
+                          // setEditCarId(id);
+                          console.log("submitted");
+                        }}>
+                        Submit
+                      </Button>
+                      <Button
+                        variant="outlined"
+                        color="error"
+                        size="small"
+                        onClick={() => setEditCarId(null)}>
+                        Cancel
+                      </Button>
+                    </Stack>
+                  </Grid>
+                </Grid>
+              )}
             </Grid>
-            <Grid item paddingRight={2}>
-              <Stack spacing={1} direction="row">
-                <Button
-                  variant="outlined"
-                  color="secondary"
-                  size="small"
-                  onClick={() => null}>
-                  Edit
-                </Button>
-                <Button
-                  variant="outlined"
-                  color="error"
-                  size="small"
-                  onClick={() => handleDeleteCar(id)}>
-                  Delete
-                </Button>
-              </Stack>
-            </Grid>
-          </Grid>
-        ))}
+          ))}
     </Container>
   );
 };
